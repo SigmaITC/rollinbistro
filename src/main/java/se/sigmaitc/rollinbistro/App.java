061aea7f73ea75c19af0aeeabbad0299d581cf73
Pelle Karlsson <per.karlsson.5@consultant.volvo.com>
2017-10-11 09:59:14 +0200

    Renamed package scan

--- src/main/java/se/sigmaitc/rollinbistro/App.java
+++ src/main/java/se/sigmaitc/rollinbistro/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan( basePackages = {"se.karlsson.thousandtwentyfive.entity"} )
+@EntityScan( basePackages = {"se.sigmaitc.rollinbistro.entity"} )
 public class App {
     
     public static void main(String[] args) {
